154e532d0b27bb0739ed0d5ca711c09110d31671
SeongYong Jeon <kibe1121@suhwooms.com>
2023-07-17 16:54:51 +0900

    일지 수정

---
+++ CameraRFID/Diary.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.IO;
+
+namespace CameraRFID
+{
+    class Diary
+    {
+        public Diary()
+        {
+
+        }
+
+        public async Task updateDiary(string job, string cardNumber, double measureWormWeight, double measureCO2, 
+            double measureNH3, double measureTEMP, double measureHUMID,string selectInstarName)
+        {
+            try
+            {
+                string requestUrl = "https://ifactoryfarm.farminsf.com/back/createDiary/{job}/{cardNumber}"
+                            .Replace("{job}", job)
+                            .Replace("{cardNumber}", cardNumber);
+
+                Console.WriteLine(requestUrl);
+
+                JObject bodyMessage = new JObject();
+
+                    JArray tags = new JArray();
+                    JArray diaryItmes = new JArray();
+
+                    JObject tagCO2 = new JObject();
+                    JObject tagNH3 = new JObject();
+                    JObject tagHUMID = new JObject();
+                    JObject tagTEMP = new JObject();
+
+                    JObject instar = new JObject();
+                    JObject wormWeight = new JObject();
+
+                    tagCO2.Add("name", "CO2");
+                    tagCO2.Add("value", measureCO2);
+                    tags.Add(tagCO2);
+
+                    tagNH3.Add("name", "NH3");
+                    tagNH3.Add("value", measureNH3);
+                    tags.Add(tagNH3);
+
+                    tagHUMID.Add("name", "습도");
+                    tagHUMID.Add("value", measureHUMID);
+                    tags.Add(tagHUMID);
+
+                    tagTEMP.Add("name", "온도");
+                    tagTEMP.Add("value", measureTEMP);
+                    tags.Add(tagTEMP);
+
+                    //생육단계
+                    instar.Add("name", "instar");
+                    instar.Add("value", selectInstarName);
+                    diaryItmes.Add(instar);
+
+                   //개체무게
+                    wormWeight.Add("name", "wormWeight");
+                    wormWeight.Add("value", measureWormWeight);
+                    diaryItmes.Add(wormWeight);
+
+
+                    bodyMessage.Add("diaryItmes", diaryItmes);
+                    bodyMessage.Add("tags", tags);
+
+
+                var client = new HttpClient();
+                var response = await sendHttpRequestAsync(client, requestUrl, bodyMessage);
+
+                // 응답 데이터 가져오기
+                var responseJson = await response.Content.ReadAsStringAsync();
+
+                // 응답 데이터 역직렬화
+                var responseData = Newtonsoft.Json.JsonConvert.DeserializeObject(responseJson);
+
+                JObject parseJSON = JObject.Parse(responseJson);
+
+                var id = (int)parseJSON["diaryId"];
+
+                //이미지 파일 업로드
+                await uploadImageAsync(id);
+
+             
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.StackTrace.ToString());
+            }
+        }
+
+        public async Task<HttpResponseMessage> sendHttpRequestAsync(HttpClient client, string requestUrl, JObject bodyMessage)
+        {
+            try
+            {
+                client.DefaultRequestHeaders.ExpectContinue = false; // <-- Make sure it is false.
+                client.Timeout = TimeSpan.FromSeconds(60);
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var httpContent = new StringContent(bodyMessage.ToString(Formatting.None), System.Text.Encoding.UTF8, "application/json");
+
+                var response = await client.PostAsync(requestUrl, httpContent);
+
+                return response;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.StackTrace.ToString());
+                return null;
+            }
+        }
+
+        public async Task uploadImageAsync(int diaryId)
+        {
+            try
+            {
+                var client = new HttpClient();
+
+                using (var form = new MultipartFormDataContent())
+                {
+                    // Get the path of the local application data folder
+                    string localAppData = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+
+                    // Define the snapshot directory
+                    string snapshotDirectory = Path.Combine(localAppData, "CAMERA_RFID", "Snapshot");
+
+                    // Define the full file path
+                    string imagePath = Path.Combine(snapshotDirectory, "Snapshot.bmp");
+
+                    if (File.Exists(imagePath))
+                    {
+                        var imageContent = new ByteArrayContent(File.ReadAllBytes(imagePath));
+                        imageContent.Headers.ContentType = MediaTypeHeaderValue.Parse("image/jpeg");
+                        form.Add(imageContent, "image", Path.GetFileName(imagePath));
+
+                        // API URL 생성
+                        string apiUrlWithParams = "https://ifactoryfarm.farminsf.com/back/ifactory/api/file/uploadfile/{uploadPath}/{id}"
+                            .Replace("{uploadPath}", "diary")
+                            .Replace("{id}", diaryId.ToString());
+
+                        // API 호출
+                        var uploadResponse = await client.PostAsync(apiUrlWithParams, form);
+                        if (uploadResponse.IsSuccessStatusCode)
+                        {
+                            var uploadResult = await uploadResponse.Content.ReadAsStringAsync();
+                            Console.WriteLine("image_upload_resut: " + uploadResult);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Error: " + uploadResponse.StatusCode);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.StackTrace.ToString());
+            }
+        }
+    }
+}
---
+++ CameraRFID/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿
+namespace CameraRFID
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.input_wormWeight = new System.Windows.Forms.TextBox();
+            this.selectInstarBox = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(391, 481);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::CameraRFID.Properties.Resources.close;
+            this.pictureBox1.Location = new System.Drawing.Point(350, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(25, 25);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // input_wormWeight
+            // 
+            this.input_wormWeight.Location = new System.Drawing.Point(32, 433);
+            this.input_wormWeight.Name = "input_wormWeight";
+            this.input_wormWeight.Size = new System.Drawing.Size(164, 21);
+            this.input_wormWeight.TabIndex = 7;
+            this.input_wormWeight.TextChanged += new System.EventHandler(this.updateWormWeight);
+            this.input_wormWeight.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.WormWeightValidation);
+            // 
+            // selectInstarBox
+            // 
+            this.selectInstarBox.AutoCompleteCustomSource.AddRange(new string[] {
+            "알",
+            "유충",
+            "번데기",
+            "성충"});
+            this.selectInstarBox.FormattingEnabled = true;
+            this.selectInstarBox.Items.AddRange(new object[] {
+            "알",
+            "유충",
+            "번데기",
+            "성충"});
+            this.selectInstarBox.Location = new System.Drawing.Point(32, 391);
+            this.selectInstarBox.Name = "selectInstarBox";
+            this.selectInstarBox.Size = new System.Drawing.Size(121, 20);
+            this.selectInstarBox.TabIndex = 13;
+            this.selectInstarBox.SelectedIndexChanged += new System.EventHandler(this.selectInstarName);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.ClientSize = new System.Drawing.Size(391, 481);
+            this.Controls.Add(this.selectInstarBox);
+            this.Controls.Add(this.input_wormWeight);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Location = new System.Drawing.Point(625, 143);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Form1";
+            this.TopMost = true;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox input_wormWeight;
+        private System.Windows.Forms.ComboBox selectInstarBox;
+    }
+}
+
